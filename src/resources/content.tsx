d72060d62df59af8c42f893151706be1246e496d
Dirge <fuaddabbad@gmail.com>
2026-09-22 15:15:40 +0700

    save

--- src/resources/content.tsx
+++ src/resources/content.tsx
@@ -324,7 +324,7 @@ const gallery: Gallery = {
   description: `A photo collection by ${person.name}`,
   images: [
     {
-      src: "https://rbsdcfuodmlgqqobekqq.supabase.co/storage/v1/object/public/bucket1/mycms/bbpvp-bandung.jpeg",
+      src: "rbsdcfuodmlgqqobekqq.supabase.co/storage/v1/object/public/bucket1/mycms/bbpvp-bandung.jpeg",
       alt: "dev-fest-bandung-2025",
       orientation: "vertical",
     },
