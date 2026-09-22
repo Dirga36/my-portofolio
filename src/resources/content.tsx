6a4953013f3ab5b6c64a2201ae51a94f961d1503
Dirge <fuaddabbad@gmail.com>
2026-09-22 15:13:14 +0700

    chore: supabase bucket test

--- src/resources/content.tsx
+++ src/resources/content.tsx
@@ -324,7 +324,7 @@ const gallery: Gallery = {
   description: `A photo collection by ${person.name}`,
   images: [
     {
-      src: "/images/gallery/dev-fest-bandung-2025.jpeg",
+      src: "https://rbsdcfuodmlgqqobekqq.supabase.co/storage/v1/object/public/bucket1/mycms/bbpvp-bandung.jpeg",
       alt: "dev-fest-bandung-2025",
       orientation: "vertical",
     },
